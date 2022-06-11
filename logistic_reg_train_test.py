705316ba82afc5e6172b9c9db9bd3e7ac3e1e31b
charlie2951 <90516512+charlie2951@users.noreply.github.com>
2022-06-11 23:34:25 +0530

    Create logistic_reg_train_test.py

---
+++ logistic_reg_train_test.py
@@ -0,0 +1,40 @@
+##Logistic Regression in Raspberry Pi Pico-RP-2040 using Micropython
+##Use small file size to avoid memory error
+##Tested using only 100 row in the diabetes.csv file, check file name, make sure
+#that 1st row which contain string is removed (string not supported)
+from pylab import logistic_regression as lm
+def csvread(file_name):  # function for reading csv file
+    f = open(file_name, 'r')
+    w = []
+    tmp = []
+    for each in f:
+        w.append(each)
+        # print (each)
+
+    # print(w)
+    for i in range(len(w)):
+        data = w[i].split(",")
+        tmp.append(data)
+        # print(data)
+    file_data = lm.transpose([[float(y) for y in x] for x in tmp])
+    # file_data = [[float(y) for y in x] for x in tmp]
+    return file_data
+####### Test function ###########################
+raw_data = csvread('diabetes_pima.csv')
+
+# Normalize data using mean and stdev()
+scaled_data = [lm.normalize(raw_data[i]) for i in range(len(raw_data[:8]))]
+xtrain, xtest, ytrain, ytest = lm.train_test_split(scaled_data,raw_data[8], 0.7)#70% data used for training
+# Xtrain data
+#Train and Build model using Logistic Regression
+w, b, loss_train, ypred_train = lm.logistic_regressor(xtrain, ytrain, 0.1, 1000)
+ypred_train = lm.predict_class(ypred_train)
+print(ypred_train)
+lm.classification_report(ytrain, ypred_train)
+#print(len(xtrain[0]))
+##test set
+#W = [0.37799744,1.06052058,-0.25524181,0.04704248,-0.1428577,0.73400824,0.32438074,0.20997864]
+#B = -0.82373324
+ypred_test = lm.predict_class(lm.evaluate_pred(w,xtest,b))
+lm.classification_report(ytest, ypred_test)
+
